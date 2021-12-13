8885fb5473d25cac1db7fbcc628258e4c84faa4a
Marcin Kasznia <kasmar00@gmail.com>
2021-12-13 10:02:52 +0000

    first battle

--- samples/java-springboot/src/main/java/hello/Application.java
+++ samples/java-springboot/src/main/java/hello/Application.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.Map.Entry;
+import java.lang.Math;
 
 @SpringBootApplication
 @RestController
@@ -56,10 +59,53 @@ public class Application {
   @PostMapping("/**")
   public String index(@RequestBody ArenaUpdate arenaUpdate) {
     System.out.println(arenaUpdate);
-    String[] commands = new String[]{"F", "R", "L", "T"};
-    int i = new Random().nextInt(4);
-    return commands[i];
+
+    String[] directions = {"N", "S", "E", "W"};
+    String[] rotations = new String[]{"R", "L"};
+    String[] rotationsAndForward = new String[]{"R", "L", "F"};
+    int i = new Random().nextInt(3);
+
+    PlayerState me = arenaUpdate.arena.state.get(arenaUpdate._links.self.href);
+    for (Entry<String,PlayerState> entry : arenaUpdate.arena.state.entrySet()) {
+        if (entry.getKey().equals(arenaUpdate._links.self.href)){
+
+        } else {
+            double dist = calcDistance(entry.getValue(), me);
+            String dir = direction(me, entry.getValue());
+            if (dist<=3 && dir.equals(me.direction)) {
+                return "T";
+            } else if (dist <=4 && dir.equals(me.direction)) {
+                return "F";
+            } else if (dist<=2 && Arrays.asList(directions).contains(dir)) {
+                // return rotateTo(me.direction, dir);
+                int choice = new Random().nextInt(2);
+                return rotations[choice];
+            }
+        }
+    }
+    return rotationsAndForward[i];
   }
+
+  public double calcDistance(PlayerState a, PlayerState b) {
+    return Math.sqrt(Math.pow(a.x-b.x,2)+Math.pow(a.y-b.y, 2));
+  }
+
+  public String direction(PlayerState a, PlayerState b) {
+      if (a.x==b.x && a.y > b.y) {
+          return "N";
+      } else if (a.x==b.x && a.y < b.y) {
+          return "S";
+      } else if (a.y==b.y && a.x < b.x) {
+          return "E";
+      } else if (a.y==b.y && a.x > b.x) {
+          return "W";
+      }
+      return "";
+  }
+
+//   public String roatetTo(String me, String other){
+
+//   }
 
 }
 
